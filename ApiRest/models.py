5614ebbf344eea1c70bd48facbdc69e0ae8eb475
Alberto <mycoworkingspain@gmail.com>
2020-03-07 18:38:26 +0100

    actualizacion models

--- ApiRest/models.py
+++ ApiRest/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+
+    WebUrl = models.CharField(max_length=50)
+    phoneNumber = models.CharField(max_length=50)
+    Email = models.CharField(max_length=100)
+
+
+class Coworking(models.Model):
+     Contact = models.ManyToManyField(Contact)
