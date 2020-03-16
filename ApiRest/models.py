dc5a6a1e4b3d2bce441b1f57512da92a63bf3604
Alberto <mycoworkingspain@gmail.com>
2020-03-16 18:22:57 +0100

    Save Contact

--- ApiRest/models.py
+++ ApiRest/models.py
@@ -2,12 +2,32 @@ from django.db import models
 
 # Create your models here.
 
+
+
+class facebook(models.Model):
+     facebookUrl = models.CharField(max_length=128)
+
+class instagram(models.Model):
+     instagramUrl = models.CharField(max_length=128)
+
+class twitter(models.Model):
+     twitterUrl = models.CharField(max_length=128)
+
+class linkedin(models.Model):
+     linkedinUrl = models.CharField(max_length=128)
+
+class socialMedia(models.Model):
+     facebook = models.ForeignKey(facebook,on_delete=models.CASCADE,null=True)
+     instagram = models.ForeignKey(instagram,on_delete=models.CASCADE,null=True)
+     twitter = models.ForeignKey(twitter,on_delete=models.CASCADE,null=True)
+     linkedin = models.ForeignKey(linkedin,on_delete=models.CASCADE,null=True)
+
 class Contact(models.Model):
 
     WebUrl = models.CharField(max_length=50)
     phoneNumber = models.CharField(max_length=50)
     Email = models.CharField(max_length=100)
-
+    socialMedia = models.ForeignKey(socialMedia,on_delete=models.CASCADE,null=True)
 
 class Coworking(models.Model):
      Contact = models.ForeignKey(Contact,on_delete=models.CASCADE)
--- ApiRest/views.py
+++ ApiRest/views.py
@@ -8,7 +8,7 @@ from django.views.decorators.csrf import csrf_exempt,csrf_protect
 from ApiRest.utils import *
 from ApiRest.models import *
 from django.core import serializers
-
+import json
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -242,12 +242,49 @@ def Test(request):
     if request.method == 'POST':
 
         try:
-        
-          b2 = Contact.objects.create(WebUrl='Cheddar Talk', phoneNumber='Thoughts on cheese.',Email='nanana')
+          # GET DATA
+          coworkings = request.data.get('coworkings', None)
 
-          Coworking_instance = Coworking(Contact=b2)
-          Coworking_instance.save()
+          First_coworking = coworkings[0]
+
+          contact_test = First_coworking["contact"]
+
+          # GET DATA
+
+          #Create Contact
+          #Create SocialMedia
+          Social_media = contact_test["socialMedia"]
+          if Social_media["facebook"]["hasFacebook"]:
+            facebook_to_save = facebook.objects.create(facebookUrl=Social_media["facebook"]["facebookUrl"])
+          else:
+            facebook_to_save = None
+          if Social_media["instagram"]["hasInstagram"]:
+            instagram_to_save = instagram.objects.create(instagramUrl=Social_media["instagram"]["instagramUrl"])
+          else:
+            instagram_to_save = None
+          if Social_media["twitter"]["hasTwitter"]:
+            twitter_to_save = twitter.objects.create(twitterUrl=Social_media["twitter"]["twitterUrl"])
+          else:
+            twitter_to_save = None
+          if Social_media["linkedin"]["hasLinkedin"]:
+            linkedin_to_save = linkedin.objects.create(linkedinUrl=Social_media["linkedin"]["linkedinUrl"])
+          else:
+            linkedin_to_save = None
+
+          SocialMedia_to_save = socialMedia.objects.create(facebook=facebook_to_save, instagram=instagram_to_save,twitter=twitter_to_save,linkedin=linkedin_to_save)
           
+          #Create SocialMedia
+          contact_to_save = Contact.objects.create(WebUrl=contact_test["webURL"], phoneNumber=contact_test["phoneNumber"],Email=contact_test["email"],socialMedia=SocialMedia_to_save)
+          #Create Contact
+          
+          #b2 = Contact.objects.create(WebUrl='Cheddar Talk', phoneNumber='Thoughts on cheese.',Email='nanana')
+
+          #Coworking create
+          Coworking_instance = Coworking(Contact=contact_to_save)
+          Coworking_instance.save()
+          #Coworking create
+
+
           #example
           ringo = Person.objects.create(name="Ringo Starr")
           paul = Person.objects.create(name="Paul McCartney")
@@ -257,16 +294,22 @@ def Test(request):
           #m1.save()
           #example
 
-          
-          Contact_arrray = Contact.objects.all()
+          # Busqueda
+
+          #Contact_arrray = Contact.objects.all()
           Coworking_arrray = Coworking.objects.all()
-          serialized_obj = serializers.serialize('json', [ Coworking_arrray[0].Contact, ])
-          Coworking_instance_search = Coworking.objects.filter(pk=Coworking_arrray[0].pk)
+          serialized_obj = serializers.serialize('json', [ Coworking_arrray[0].Contact.socialMedia, ])
+          #Coworking_instance_search = Coworking.objects.filter(pk=Coworking_arrray[0].pk)
           #serialized_obj = serializers.serialize('json', [ Coworking_instance_search.first().Contact, ])
 
-          Contact_instance = Contact.objects.filter(pk=Contact_arrray[1].pk)
+          #Contact_instance = Contact.objects.filter(pk=Contact_arrray[1].pk)
           #serialized_obj = serializers.serialize('json', [ Contact_instance.first(), ])
-          return Response({serialized_obj},status=200)
+          return Response(json.loads(serialized_obj),status=200)
+
+           # Busqueda
+
+
+          return Response(contact_test,status=200)
 
         except Exception as e:
 
