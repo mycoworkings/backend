c415400d05ff629e4fcccf54f5a2be316689401e
Bernat Rivera <rivera.rodriguez.bernat@gmail.com>
2020-03-25 19:59:52 +0100

    Added AWS datasource

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
-#DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.mysql',
-#            'NAME': "coworkings",
-#            'USER': "admin",
-#            'PASSWORD': "piscinadebilletes",
-#            'HOST': "test-db.c5l5ifiugqzm.us-east-2.rds.amazonaws.com",
-#            'PORT': 3306,
-#        }
-#    }
+DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.mysql',
+           'NAME': "coworkings",
+           'USER': "admin",
+           'PASSWORD': "piscinadebilletes",
+           'HOST': "test-db.c5l5ifiugqzm.us-east-2.rds.amazonaws.com",
+           'PORT': 3306,
+       }
+   }
 
 
 # Password validation
